2f7f1b724b87622e5c00afe4cf3c0d0c596c14b0
victor-alvescv <victoralvescabral7@gmail.com>
2022-08-30 17:05:30 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// ADD LISTENER EVENT
+
+let box = document.getElementById("box")
+
+box.addEventListener("click", function() { // will take out the onclick() from the html
+    console.log("I want to open the box!")
+})
+
+
